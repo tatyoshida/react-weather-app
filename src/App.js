f0c00dc81541deabe41748ef61b9b841d097bdd6
Tatiana Yoshida <tatyoshida@gmail.com>
2024-01-27 15:37:12 -0300

    Updated footer links & style

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       Weather app
+      <footer>
+        This project was coded by <a href="https://github.com/tatyoshida" target="_blank" rel="noreferrer">Tatyoshida</a> is <a href="https://github.com/tatyoshida/react-weather-app" target="_blank" rel="noreferrer">open-sorced</a> and hosted on <a href="https://curious-selkie-c7cdbb.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>.
+      </footer>
     </div>
   );
 }
