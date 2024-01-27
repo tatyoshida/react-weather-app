d8c11ec64e9f0a30e6cd9bf1519a1260fea5954b
Tatiana Yoshida <tatyoshida@gmail.com>
2024-01-27 16:27:38 -0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        This project was coded by <a href="https://github.com/tatyoshida" target="_blank" rel="noreferrer">Tatyoshida</a> is <a href="https://github.com/tatyoshida/react-weather-app" target="_blank" rel="noreferrer">open-sorced</a> and hosted on <a href="https://curious-selkie-c7cdbb.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>.
-      </footer>
+      <div className="container">
+        Weather app
+        <footer>
+          This project was coded by <a href="https://github.com/tatyoshida" target="_blank" rel="noreferrer">Tatyoshida</a> is <a href="https://github.com/tatyoshida/react-weather-app" target="_blank" rel="noreferrer">open-sorced</a> and hosted on <a href="https://curious-selkie-c7cdbb.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>.
+        </footer>
+      </div>
     </div>
   );
 }
