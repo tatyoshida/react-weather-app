86d75e76fb6d91910fe2eedf9cc601e37839399d
Tatiana Yoshida <tatyoshida@gmail.com>
2024-01-30 23:59:36 -0300

    Added responsive style

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -10,20 +10,20 @@ export default function WeatherInfo(props){
       </h3>
       <div className="CurrentWeather">
         <div className="row">
-          <div className="col-6 Description">
+          <div className="col-sm-6 Description">
             <div className="Icon">
               <img src={props.data.iconUrl} alt={props.data.description}></img>
             </div>
             <span className="text-capitalize">{props.data.description}</span>
           </div>
-          <div className="col-6 Temperature">
+          <div className="col-sm-6 Temperature">
                 <TemperatureValue celsius= {props.data.temperature} />
           </div>
         </div>  
         <div className="row WeatherDetails">
-          <div className="col-4 Pressure"><span>Presión: {props.data.pressure} mbar</span></div>
-          <div className="col-4 Humidity"><span>Humedad: {props.data.humidity} %</span></div>
-          <div className="col-4 Wind"><span>Viento: {Math.round(props.data.wind)} Km/h</span></div>                      
+          <div className="col-sm-4 Pressure"><span>Presión: {props.data.pressure} mbar</span></div>
+          <div className="col-sm-4 Humidity"><span>Humedad: {props.data.humidity} %</span></div>
+          <div className="col-sm-4 Wind"><span>Viento: {Math.round(props.data.wind)} Km/h</span></div>                      
         </div>  
       </div>
     </div>
